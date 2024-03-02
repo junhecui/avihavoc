9b79a1df32912b38985e99075cd0339632a24046
junhe-c <cjunhe05@gmail.com>
2024-03-02 10:43:27 -0800

    basic app.py

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flightradar24 import FlightRadar24API
+from retry import retry
+
+fr_api = FlightRadar24API()
+
+'''
+info needed: 
+- latitude, longitude of planes (for putting on site)
+- type of plane (for emission calculations)
+- airline (for sorting purposes)
+'''
+flights = fr_api.get_flights()  # Returns a list of Flight objects
+
+# flight = fr_api.get_flight("N421UW")
+# print(flight)
+# airport = "KJFK"
+# distance = flight.get_distance_from(airport, details=True)
+# print(distance)
+
+for flight in flights:
+    flight_details = fr_api.get_flight_details(flight)
+    flight.set_flight_details(flight_details)
+
+    print("Flying to", flight.destination_airport_name)
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_tempalte
+from flask import Flask, render_template
 from flightradar24 import FlightRadar24API
 
 app = Flask(__name__)
@@ -11,9 +11,8 @@ def display():
     print('hello world')
     pass
     #return render_template('nameof.html')
-    
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000,debug=True)
 
-    app.run(host='0.0.0.0', port=5000,debug=True)
 
