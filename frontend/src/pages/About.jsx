bb01930471df4f03af71450188a4bbf82cfe14e1
ShafeiW <shafei.waqar@icloud.com>
2024-03-02 15:51:09 -0800

    hyperlink in about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,8 +5,16 @@ export function About() {
     <div className="about">
       <div className="about-container">
         <p className="about-bigheader">About Us.</p>
-        <p className="about-littlecaption">Front End: Yejun & Shafei.</p>
-        <p className="about-littlecaption">Back End: Liz & Jun He.</p>
+        <p className="about-littlecaption">
+          Front End:
+          <a className='about-underline' href="https://github.com/ShafeiW"> Shafei </a> & 
+          <a className='about-underline' href="https://github.com/unsignd"> Yejun </a>
+          </p>
+        <p className="about-littlecaption">
+          Back End: 
+          <a className= 'about-underline' href="https://github.com/lizard777"> Liz </a> & 
+          <a className= 'about-underline' href="https://github.com/junhe-c"> Jun He </a>
+          </p>
         <p className="about-littlecaption2">
           Passionate computer science students with a thirst for learning.
         </p>
