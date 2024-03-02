e06be281f532cc8499fa4552abbafe09bb85f12c
lizard777 <79130777+lizard777@users.noreply.github.com>
2024-03-02 14:11:19 -0800

    adding venv of flask

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_tempalte
-from flightradar24 import FlightRadar24API
+from flask import Flask, render_template
+from FlightRadar24 import FlightRadar24API
+import csv
+import requests
+from firebase import firebase
 
+firebase = firebase.FirebaseApplication('https://flightdb-55075-default-rtdb.firebaseio.com/', None)
 app = Flask(__name__)
 
 def carbon_calc(km):
@@ -8,6 +12,8 @@ def carbon_calc(km):
 
 @app.route('/')
 def display():
+    result = firebase.get('/Flight',None)
+    print(result)
     print('hello world')
     pass
     #return render_template('nameof.html')
@@ -17,3 +23,10 @@ if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=5000,debug=True)
 
+
+
+# Activate the virtualenv (OS X & Linux)
+#$ source myproject/bin/activate
+
+# Activate the virtualenv (Windows)
+#$ myproject\Scripts\activate
