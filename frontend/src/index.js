e8668b1703bb4e3ec9d0fd86ac93a23ac8869bf6
unsignd <unsigndid@gmail.com>
2024-03-02 12:32:54 -0800

    add globe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import { Landing } from './pages/Landing';
 import { Header } from './components/Header';
+import { Map } from './pages/Map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +13,7 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/map" element={<Landing />} />
+        <Route path="/map" element={<Map />} />
         <Route path="/credit" element={<Landing />} />
         <Route path="*" element={<p>ahh</p>} />
       </Routes>
